2af329e63792e685d3bafb65d5879a65514ead4a
Felipe P <fp.limapereira@gmail.com>
2020-08-31 18:41:48 -0300

    Tutorial Vídeo 2
    O que foi implementado:
    
    -Configuração de gradle incluindo as dependências para coroutines, room, injeção de dependência com Kodein, arquitetura de navegação, material design, viewmodel e livedata, Retrofit, Gson e dataBiding;
    
    -Definido os diretórios de data, ui e auth;
    
    -Incluído as activities de login e signup, junto com seus layouts;
    
    -Implementado valores de cores e estilos;

---
+++ app/src/main/java/com/flpereira/projetomvvm/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.flpereira.projetomvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flpereira.projetomvvm.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
